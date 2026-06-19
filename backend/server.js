6c338d2a532efeaee7fadebc9309d01f299ca7c9
xefoxxtv <legox.esport@gmail.com>
2026-06-19 13:32:06 +0200

    fix

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const MongoStore = require('connect-mongo').default || require('connect-mongo');
 const axios = require('axios');
 const cors = require('cors');
 const path = require('path');
